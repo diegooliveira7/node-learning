390d9569406349d99dac2520f53b9d276cde2136
diegoolliveira7 <diegoolliveira1999@gmail.com>
2024-08-27 22:27:31 -0300

    conexão com o mongo

--- node-crud-mongodb/src/index.js
+++ node-crud-mongodb/src/index.js
@@ -1,9 +1,32 @@
 const express = require("express")
+const mongoose = require('mongoose')
+
 const app = express()
+app.use(express.json())
 const port = 3000
+mongoose.connect('mongodb+srv://dpdo1:adm123@node-mongo.8iq2q.mongodb.net/?retryWrites=true&w=majority&appName=node-mongo')
+
+const Movie = mongoose.model('Movie', {
+    title: String,
+    description: String,
+    image_url: String,
+    trailer_url: String
+});
 
 app.get("/", (rep, res) => {
     res.send("Hello World");
+});
+
+app.post("/", async (req, res) => {
+    const movie = new Movie({
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        trailer_url: req.body.trailer_url
+    });
+
+    await movie.save();
+    res.send(movie);
 });
 
 app.listen(port, () => {

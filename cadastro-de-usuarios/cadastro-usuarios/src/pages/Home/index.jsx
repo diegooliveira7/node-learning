68ad9a07507840b36017150ff3f002d20ef0009f
diegoolliveira7 <diegoolliveira1999@gmail.com>
2024-09-10 22:02:56 -0300

    teste

--- cadastro-de-usuarios/cadastro-usuarios/src/pages/Home/index.jsx
+++ cadastro-de-usuarios/cadastro-usuarios/src/pages/Home/index.jsx
@@ -2,17 +2,47 @@ import './style.css'
 
 function Home() {
 
+  const users = [
+    {
+      id: '225151',
+      name: 'Diego',
+      age: 33,
+      email: 'diego@gamil.com'
+    },
+    {
+      id: '225152',
+      name: 'Suze',
+      age: 3,
+      email: 'suze@gamil.com'
+    }
+  ]
+
   return (
         
+    <>
     <div className='container'>
       <form>
         <h1>User Registration</h1>
-        <input name='name' type="text"/>
-        <input name='age' type="number"/>
-        <input name='email' type="email"/>
+        <input name='name' type="text" />
+        <input name='age' type="number" />
+        <input name='email' type="email" />
         <button type='button'>Register</button>
       </form>
+
+      {users.map((user) => (
+        <>
+        <div key={user.id}>
+          <p>Name: {user.name}</p>
+          <p>Age: {user.age}</p>
+          <p>Email: {user.email}</p>
+        </div>
+        <button>
+
+        </button>
+        </>
+      ))}
     </div>
+    </>
         
       
     
